a8c1dfe5a6c62ff4ffb24760a7d42c5cdf5d65fb
Thomas M. Hesselink <mail@t64.nl>
2023-04-27 12:52:45 +0200

    bugfix, camera motion at startup.

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,6 +1,11 @@
 extends Camera2D
 
 
+func _init():
+	position_smoothing_enabled = false
+	limit_smoothed = false
+
+
 func _ready():
 	var tile_map: TileMap = get_parent().get_parent().get_node("TileMap")
 	var map_rect: Rect2i = tile_map.get_used_rect()
@@ -8,4 +13,6 @@ func _ready():
 	var map_size_in_px: Vector2i = map_rect.size * tile_size
 	limit_right = map_size_in_px.x
 	limit_bottom = map_size_in_px.y
+	limit_smoothed = true
+	position_smoothing_enabled = true
 
