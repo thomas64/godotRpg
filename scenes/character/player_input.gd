a3c65ff46988957b7e0827c08a0ffd5a51ca952c
Thomas M. Hesselink <mail@t64.nl>
2023-05-29 22:01:07 +0200

    player input refactoring.

--- scenes/character/player_input.gd
+++ scenes/character/player_input.gd
@@ -27,12 +27,16 @@ func _ready():
 func update(delta):
 	if _input_delay > 0:
 		_input_delay -= delta
-		return
+	else:
+		_process_input(delta)
+
+
+func _process_input(delta):
 	_process_key_input()
-	_set_move_speed()
-	_set_turn_delay()
-	_set_direction()
-	_set_velocity(delta)
+	_turn_delay = _get_turn_delay(delta)
+	move_speed = _get_move_speed()
+	direction = _get_direction()
+	velocity = _get_velocity()
 
 
 func _process_key_input():
@@ -45,61 +49,58 @@ func _process_key_input():
 	_press_shift = Input.is_action_pressed("player_run")
 
 
-func _set_move_speed():
-	if !_are_move_keys_pressed():
-		move_speed = Constant.MOVE_SPEED_0
-	elif _press_up and _press_down:
-		move_speed = Constant.MOVE_SPEED_0
-	elif _press_left and _press_right:
-		move_speed = Constant.MOVE_SPEED_0
-	elif _press_ctrl and _press_shift and Globals.is_in_debug_mode:
-		move_speed = Constant.MOVE_SPEED_4
-	elif _press_shift:
-		move_speed = Constant.MOVE_SPEED_3
-	elif _press_ctrl:
-		move_speed = Constant.MOVE_SPEED_1
-	else:
-		move_speed = Constant.MOVE_SPEED_2
-
-
-func _set_turn_delay():
+func _get_turn_delay(delta) -> float:
 	if _turn_delay <= 0 and velocity == Vector2.ZERO and _is_key_press_other_direction():
-		_turn_delay = _TURN_DELAY_TIME
-
-	if !_are_move_keys_pressed():
-		_turn_delay = 0
-
-
-func _set_direction():
-	if _press_up and _press_down:
-		direction = Direction.SOUTH
-	elif _press_left and _press_right:
-		direction = Direction.SOUTH
-	elif _press_left:
-		direction = Direction.WEST
-	elif _press_right:
-		direction = Direction.EAST
-	elif _press_up:
-		direction = Direction.NORTH
-	elif _press_down:
-		direction = Direction.SOUTH
-
-
-func _set_velocity(delta):
-	if _are_move_keys_pressed():
-		if _turn_delay > 0:
-			_turn_delay -= delta
-			move_speed = Constant.MOVE_SPEED_0
-		else:
-			var input_direction: Vector2 = Input.get_vector("player_west", "player_east", "player_north", "player_south")
-			var angle: float = deg_to_rad(round(rad_to_deg(input_direction.normalized().angle()) / 45.0) * 45.0)
-			velocity = Vector2(cos(angle), sin(angle)) * move_speed
+		return _TURN_DELAY_TIME
+	elif !_are_move_keys_pressed():
+		return 0
 	else:
-		velocity = Vector2.ZERO
+		return _turn_delay - delta
+
+
+func _get_move_speed() -> int:
+	if !_are_move_keys_pressed() or _are_opposite_keys_pressed() or _turn_delay > 0:
+		return Constant.MOVE_SPEED_0
+	elif _press_ctrl and _press_shift and Globals.is_in_debug_mode:
+		return Constant.MOVE_SPEED_4
+	elif _press_shift:
+		return Constant.MOVE_SPEED_3
+	elif _press_ctrl:
+		return Constant.MOVE_SPEED_1
+	else:
+		return Constant.MOVE_SPEED_2
+
+
+func _get_direction() -> int:
+	if _are_opposite_keys_pressed():
+		return direction
+	elif _press_left:
+		return Direction.WEST
+	elif _press_right:
+		return Direction.EAST
+	elif _press_up:
+		return Direction.NORTH
+	elif _press_down:
+		return Direction.SOUTH
+	else:
+		return direction
+
+
+func _get_velocity() -> Vector2:
+	if _are_move_keys_pressed() and !_are_opposite_keys_pressed() and _turn_delay <= 0:
+		var input_direction: Vector2 = Input.get_vector("player_west", "player_east", "player_north", "player_south")
+		var angle: float = deg_to_rad(round(rad_to_deg(input_direction.normalized().angle()) / 45.0) * 45.0)
+		return Vector2(cos(angle), sin(angle)) * move_speed
+	else:
+		return Vector2.ZERO
 
 
 func _are_move_keys_pressed() -> bool:
 	return _press_up or _press_down or _press_left or _press_right
+
+
+func _are_opposite_keys_pressed() -> bool:
+	return (_press_up and _press_down) or (_press_left and _press_right)
 
 
 func _is_key_press_other_direction() -> bool:
