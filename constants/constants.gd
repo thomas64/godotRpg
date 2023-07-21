e958181c77486b78e9974356a251e19a68aa2099
Thomas M. Hesselink <mail@t64.nl>
2023-07-21 19:13:55 +0200

    use of constants.

--- constants/constants.gd
+++ constants/constants.gd
@@ -21,3 +21,5 @@ const HALF_TILE_SIZE: int = 24
 
 const FADE_TIME: float = 0.5
 
+const FOG_OF_WAR_GRID_SIZE: float = 100.0
+
--- scenes/world/fog_of_war.gd
+++ scenes/world/fog_of_war.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 
 
 const _TEXTURE = preload("res://assets/lightmaps/fog_of_war.png")
-const _GRID_SIZE = 24
+const _ZOOM_SIZE_OF_LIGHT = 24
 
 var _blackness: Image
 var _light_image: Image
@@ -10,7 +10,7 @@ var _light_offset = Vector2(_TEXTURE.get_width() / 2.0, _TEXTURE.get_height() / 
 
 
 func _ready():
-	$fog_image.scale = Vector2(_GRID_SIZE, _GRID_SIZE)
+	$fog_image.scale = Vector2(_ZOOM_SIZE_OF_LIGHT, _ZOOM_SIZE_OF_LIGHT)
 	_light_image = _TEXTURE.get_image()
 	_light_image.convert(Image.FORMAT_RGBAH)
 
@@ -18,16 +18,16 @@ func _ready():
 func on_show(tile_map: TileMap):
 	if visible:
 		var map_size = tile_map.get_size()
-		var black_width = map_size.x / _GRID_SIZE
-		var black_height = map_size.y / _GRID_SIZE
+		var black_width = map_size.x / _ZOOM_SIZE_OF_LIGHT
+		var black_height = map_size.y / _ZOOM_SIZE_OF_LIGHT
 		_blackness = Image.create(black_width, black_height, false, Image.FORMAT_RGBAH)
 		_blackness.fill(Color.BLACK)
 		for position in Globals.fog_of_war[tile_map.get_parent().name]:
-			_draw_light(position * 100)
+			_draw_light(position * Constant.FOG_OF_WAR_GRID_SIZE)
 
 
 func _draw_light(position: Vector2):
-	var grid_position = position / _GRID_SIZE
+	var grid_position = position / _ZOOM_SIZE_OF_LIGHT
 	var light_rect = Rect2(Vector2.ZERO, Vector2(_light_image.get_width(), _light_image.get_height()))
 	_blackness.blend_rect(_light_image, light_rect, grid_position - _light_offset)
 	$fog_image.texture = ImageTexture.create_from_image(_blackness)
